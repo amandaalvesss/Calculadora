9cd96ad6c6e27a77272edc43d7893eb7a2beda90
amandaalvesss <amandaalcantara98@hotmail.com>
2023-11-01 14:13:07 -0300

    first commit

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { CalculadoraModule } from './calculadora';
+describe('AppComponent', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule, CalculadoraModule],
+    declarations: [AppComponent]
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+
+});
